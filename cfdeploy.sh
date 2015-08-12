4beae2c7f4adca08dd0ad48789d46247b297b582
Kishore Bhatia <bhatia.kishore@gmail.com>
2015-08-12 17:52:17 -0400

    Update cfdeploy.sh

--- cfdeploy.sh
+++ cfdeploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cf api api.hackney.cf-app.com --skip-ssl-validation
-cf login -u kbhatia@cloudbees.com -p Jenkins!23 -o cloudbees -s development
 cf push petclinic -p target/petclinic.war
